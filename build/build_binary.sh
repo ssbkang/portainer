fab9151f57f48a5f1f18fe94f1924350ff340d9c
Steven Kang <skan070@gmail.com>
2020-03-18 13:34:28 +1300

    Update build_binary.sh

--- build/build_binary.sh
+++ build/build_binary.sh
@@ -1,16 +1,19 @@
-OS=$1
+PLATFORM=$1
 ARCH=$2
+
+echo $PLATFORM
+echo $ARCH
 
 mkdir -p dist
 
 cd 'api/cmd/portainer'
 
 go get -t -d -v ./...
-GOOS="${OS}" GOARCH="${ARCH}" CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
+GOOS="${PLATFORM}" GOARCH="${ARCH}" CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
 
 if [ "${OS}" == "linux" ]; then
   binary="portainer"
-elif [ "${OS}" == "windows" ]; then
+else
   binary="portainer.exe"
 fi
 
