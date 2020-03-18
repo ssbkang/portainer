97b11e69a3acfcbfd0fe338ff60a7519537c201e
Steven Kang <skan070@gmail.com>
2020-03-18 13:06:37 +1300

    Update build_binary.sh

--- build/build_binary.sh
+++ build/build_binary.sh
@@ -6,4 +6,4 @@ cd 'api/cmd/portainer'
 go get -t -d -v ./...
 GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags '-s'
 
-mv "${binary}" "../../../dist/portainer"
+# mv "${binary}" "../../../dist/portainer"
